d961b589d1c98b8213688769d7e162102e2b48f2
annavanwingerden <91270891+annavanwingerden@users.noreply.github.com>
2025-06-26 23:51:40 +0100

    fix:cookies

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -3,15 +3,5 @@ import { createBrowserClient } from '@supabase/ssr';
 export const createClient = () =>
   createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        name: 'sb-access-token', // You can use the default or your own name
-        lifetime: 60 * 60 * 24 * 7, // 1 week
-        domain: process.env.NEXT_PUBLIC_COOKIE_DOMAIN || undefined, // Set this in your .env for production
-        path: '/',
-        sameSite: 'Lax',
-        secure: true,
-      },
-    }
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
